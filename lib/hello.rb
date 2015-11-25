9fd9b04a664dc34054384119e3dae8f4aac8a9de
mfordyce <mfordyce97@gmail.com>
2015-11-24 21:03:33 -0800

    Hello users Greeter

--- lib/hello.rb
+++ lib/hello.rb
@@ -1,10 +1,14 @@
+require 'greeter'
+
+
 # Default is "World"
 # Author: Mark Fordyce (mfordyce97@gmail.com)
 
 
 name = ARGV.first || "World"
 
-puts "Hello, #{name}!"
+greeter = Greeter.new(name)
+puts greeter.greet
 
 
 
